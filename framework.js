ad54b6bd5dc18fe2f14d9b06897e895446f030f8
Isaac <isaac@isaackarth.com>
2018-11-02 14:23:09 -0700

    Added example separate grammar files

--- framework.js
+++ framework.js
@@ -1,19 +1,45 @@
 'use strict';
 
-let test_rules = {
-    "origin":
-    ["[protagonist:#fruit#][description:#adjective#]The three #description# #protagonist.s# say hello."],
-    "fruit":
-		["apple", "orange", "grape", "pea"],
-    "adjective":
-		["sleeping","laughing","watching"]};
+function partial(func, ...argsBound) {
+  return function(...args) { // (*)
+    return func.call(this, ...argsBound, ...args);
+  }
+}
+
+function loadJSON(callback, filepath) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', filepath, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+}
 
 
-function testGrammar(grammar_rules=test_rules, context) {
+function testGrammar(grammar_rules, context) {
+    console.log(grammar_rules);
     let grammar = tracery.createGrammar(grammar_rules);
     //grammar.addModifiers(baseEngModifiers);
     grammar.clearState();
-    var message = grammar.flatten("#origin#");
+    let message = grammar.flatten("#origin#");
     return {"message": message, "context": grammar};
 }
 
+function displayResults(input_grammar, element_id) {
+      var container = document.getElementById(element_id);
+      var responses = "<ul>";
+      for(var i = 0; i < 15; i++) {
+                         responses += "<li>";
+                         responses += testGrammar(input_grammar)["message"];
+                         responses += "</li>"
+                         }
+      responses += "</ul>";
+    container.innerHTML = responses;
+    console.log(responses);
+}
+    
---
+++ grammars/test_grammar.js
@@ -0,0 +1,7 @@
+let test_grammar = {
+    "origin":
+    ["[protagonist:#fruit#][description:#adjective#]The three #description# #protagonist.s# say hello."],
+    "fruit":
+		["apple", "orange", "grape", "pea"],
+    "adjective":
+		["sleeping","laughing","watching"]};
